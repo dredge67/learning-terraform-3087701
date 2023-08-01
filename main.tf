a45f6609ffd818f7dbda1910c4d7a18f238c2d45
dredge67 <david.reimertz@gmail.com>
2023-08-01 11:58:34 -0400

    Fixes for vpc module

--- main.tf
+++ main.tf
@@ -50,11 +50,11 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  name    = "blog"
 
   # Change this to use vpc module
   # vpc_id = data.aws_vpc.default.id
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
